e469a6f049b7f26cc12af5397358651a4208b44f
Touseef Ashraf <57070097+touseefashraf@users.noreply.github.com>
2020-04-09 15:48:09 +0500

    undo span

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -19,7 +19,7 @@ var speakWord = "Good Bye";
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 byeSpeaker.speak=function (name) {
-  console.log("<span>"+speakWord + " " + name+"</span>");
+  console.log(speakWord + " " + name);
 }
 window.byeSpeaker=byeSpeaker;
 })(window);
