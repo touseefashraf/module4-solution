8d0dcb62b486b68ef989d170bf417e664669d00a
Touseef Ashraf <57070097+touseefashraf@users.noreply.github.com>
2020-04-09 15:47:32 +0500

    undo p tag

--- SpeakHello.js
+++ SpeakHello.js
@@ -16,7 +16,7 @@ var speakWord = "Hello";
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 helloSpeaker.speak=function(name) {
-  console.log("<p>"+speakWord + " " + name+"</p>");
+  console.log(speakWord + " " + name);
 }
 window.helloSpeaker=helloSpeaker;
 })(window);
