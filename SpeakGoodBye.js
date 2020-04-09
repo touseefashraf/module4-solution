47dbff0a6ebf3004dc776576b9a17a8b052d7622
Touseef Ashraf <57070097+touseefashraf@users.noreply.github.com>
2020-04-09 15:34:18 +0500

    Span tag

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -19,7 +19,7 @@ var speakWord = "Good Bye";
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 byeSpeaker.speak=function (name) {
-  console.log(speakWord + " " + name);
+  console.log("<span>"+speakWord + " " + name+"</span>");
 }
 window.byeSpeaker=byeSpeaker;
 })(window);
